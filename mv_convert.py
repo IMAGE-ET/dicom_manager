2e1df27ef5bf1b58342a2951320549723c584e9f
MRI Lab <mrilab@NPI-A7364-GB5.local>
2015-03-03 13:01:12 -0800

    Added MV file structure description to mv_convert.
    Also assigned some variables to make it easier to build
    MV files.

--- mv_convert.py
+++ mv_convert.py
@@ -1,3 +1,27 @@
 import os
 import sys
+from struct import *
 from dataset.dataset import DicomDataset
+
+"""
+
+MedVision file structure is as follows:
+
+	-MedVision study header (24 bytes)
+		-Consists of mv_head_1 + number of slices (2 bytes) + mv_head_2
+	-Slice header (10 Bytes)
+		-Consists of #Rows(2 bytes) + #Columns(2 bytes) + sl_head_part 8 or 16 
+		 depending on data + 2 bytes of zeros (two_zeros) + slice number (2 bytes)
+	-Pixel data
+	-Slice header
+	-Pixel data
+	-etc.
+	
+	
+
+"""
+mv_head_1 = 'MVSTUDY \x00\x00\x00x\x00\x00\x00\x18\'
+mv_head_2 = '\x00\n\x00\x00\x00\x00'
+sl_head_part_8bit = ' \x01'
+sl_head_part_16bit = '\x10\x02'
+two_zeros = '\x00\x00'
